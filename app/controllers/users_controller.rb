11647542587ea5aa97ce1a21a03fdbf1aebbf468
waldowred5 <dwaldow@me.com>
2021-01-28 14:52:17 +1100

    Added sign-in method to users controller, updated routes, included seed data for Users and subscriptions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
     def create
         @user = User.create user_params
-        # if @user.save
-        #     render json: @user, status: :created
-        # else
-        #     render @user.errors, status: :unprocessable_entity
-        # end
+        if @user.save
+            auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+            render json: {first_name: @user.first_name, jwt: auth_token.token}, status: :created
+        else
+            render @user.errors, status: :unprocessable_entity
+        end
+    end
+
+    def sign_in
+        @user = User.find_by_email(params[:email])
+        if @user && @user.authenticate(params[:password])
+            auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+            render json: {first_name: @user.first_name, jwt: auth_token.token}, status: 200
+        else
+            render json: {error: 'Incorrect Email or Password'}, status: 404
+        end
     end
 
     private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if User.count == 0
+    User.create(first_name: 'John', email: 'john@example.com', password: '123456', password_confirmation: '123456')
+    User.create(first_name: 'Jane', email: 'jane@example.com', password: '1234567', password_confirmation: '1234567')
+end
+
+if Subscription.count == 0
+    Subscription.create(user_id: 1, name: 'Netflix', logo_image: 'netflix.com', amount: 2400, description: 'TV and Movie Streaming Platform')
+    Subscription.create(user_id: 2, name: 'Spotify', logo_image: 'spotify.com', amount: 1500, description: 'Music Streaming Platform')
+end
